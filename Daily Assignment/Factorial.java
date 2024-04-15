8b6952aada786c0dcaf95fb08aaf4cccbeb55059
SnehalSabnis1 <166944956+SnehalSabnis1@users.noreply.github.com>
2024-04-15 08:08:39 +0530

    Add files via upload

---
+++ Daily Assignment/Factorial.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Factorial 
+{
+  public static void main(String args[])
+  {
+   Scanner sc = new Scanner(System.in);
+   int i,n,fact=1;
+   System.out.println("Enter the number to do factorial ");
+   n = sc.nextInt();
+   for(i = 1; i <= n; i++) 
+   {
+    fact = fact*i;
+   }
+   System.out.println("Factorial="+fact);
+  }
+
+}
